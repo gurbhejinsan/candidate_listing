7856c573605b791630ca12593906cd1f80630d93
Gurbhej Insan <gurbhehinsan111@gmail.com>
2024-08-17 20:11:42 +0530

    Change some code in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,10 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   build:{
-    outDir:'dist'
+    outDir:'dist',
+
   },
+  base:"/candidate_listing",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
